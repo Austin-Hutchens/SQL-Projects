e60b37da6f37d7683b7e7a1542a16fc3818f0b6e
Austin-Hutchens <austinjhutchens@gmail.com>
2025-01-30 16:07:51 -0500

    Update employee_project_code.sql

--- Employee and Project Analysis/employee_project_code.sql
+++ Employee and Project Analysis/employee_project_code.sql
@@ -1,3 +1,5 @@
+-- 1. Select the first 5 records from the employees and projects table
+
 SELECT * 
 FROM employees
 LIMIT 5;
@@ -6,9 +8,13 @@ SELECT *
 FROM projects
 LIMIT 5;
 
+-- 2. Select the first and last names of employees who are not assigned to any project
+
 SELECT first_name, last_name
 FROM employees
 WHERE current_project IS NULL;
+
+-- 3. Select the project ID and project name where the project ID is not assigned to any employee
 
 SELECT project_id, project_name
 FROM projects
@@ -18,6 +24,9 @@ WHERE project_id NOT IN (
   WHERE current_project IS NOT NULL
 );
 
+
+-- 4. Select the project name and the count of employees working on that project, ordered by the highest count
+
 SELECT p.project_name, COUNT(*)
 FROM projects p
 JOIN employees e ON e.current_project = p.project_id
@@ -25,6 +34,9 @@ WHERE e.current_project NOT NULL
 GROUP BY p.project_name
 ORDER BY COUNT(*) DESC
 LIMIT 1;
+
+
+-- 5. Select the project name where more than one employee is assigned, ordered by the highest count
 
 SELECT p.project_name
 FROM projects p
@@ -34,6 +46,9 @@ GROUP BY p.project_name
 HAVING COUNT(e.current_project) > 1
 ORDER BY COUNT(*) DESC;
 
+
+-- 6. Calculate a custom count based on employees working on projects and developers' count
+
 SELECT (COUNT(*) * 2) - (
   SELECT COUNT(*)
   FROM employees
@@ -42,11 +57,18 @@ SELECT (COUNT(*) * 2) - (
 ) AS 'Count'
 FROM projects;
 
+
+-- 7. Select the most common personality type among employees
+
+SELECT (COUNT(*) * 2) - (
 SELECT personality
 FROM employees
 GROUP BY personality
 ORDER BY COUNT(personality) DESC
 LIMIT 1;
+
+
+-- 8. Select the project name where the most common personality type is assigned to employees
 
 SELECT project_name
 FROM projects p
@@ -59,6 +81,10 @@ WHERE personality = (
   LIMIT 1
 );
 
+
+-- 9. Select first name, last name, personality, and project name for employees with the most common personality type
+
+SELECT project_name
 SELECT e.first_name, e.last_name, e.personality, p.project_name
 FROM employees e 
 JOIN projects p ON e.current_project = p.project_id
@@ -70,6 +96,9 @@ WHERE personality = (
   ORDER BY COUNT(personality) DESC
   LIMIT 1
 );
+
+
+-- 10. Select first name, last name, personality, project name, and the count of incompatible personalities
 
 SELECT e.first_name, e.last_name, e.personality, p.project_name,
 CASE
